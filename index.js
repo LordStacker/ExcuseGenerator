f05c5ed49fd230bda528dd16d9e2d80aa36fad39
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-08-04 21:53:12 -0400

    updated with func

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let who = ["My dog", "My Cat", "My T-rex", "My pope"]
-let action = [" Explode", " burned", " ate", " Broked"]
+let who = ["My dog", "My Cat", "My T-rex", "The pope"]
+let action = [" Explode", " burned", " ate", " Broke"]
 let what = [" my shorts", " my food", " my homework", " my hair"]
 let when = [" yesterday", " this morning", " this noon"]
-const excuse1 = Math.floor(Math.random() * who.length)
-const excuse2 = Math.floor(Math.random() * action.length)
-const excuse3 = Math.floor(Math.random() * what.length)
-const excuse4 = Math.floor(Math.random() * when.length)
-console.log(who[excuse1] + action[excuse2] + what[excuse3] + when[excuse4])
+const randomnize = () => {
+    let who1 = Math.floor(Math.random() * (who.length-1)+1)
+    let action1 = Math.floor(Math.random() * (action.length-1)+1)
+    let what1 = Math.floor(Math.random() * (what.length-1)+1)
+    let when1 = Math.floor(Math.random() * (when.length-1)+1)
+    return who[who1]+action[action1]+what[what1]+when[when1]
+
+}
+console.log(randomnize())
+
+//console.log(who[excuse1] + action[excuse2] + what[excuse3] + when[excuse4])
